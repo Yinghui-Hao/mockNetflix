6aa3828fd6864b87cb46962fd96c2c24db3def9b
Yinghui-Hao <haoyinghui0426@gmail.com>
2019-01-31 10:07:39 -0800

    Add logo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import Row from "../Row";
-import Bottom from "../Bottom"
+import Bottom from "../Bottom";
 
 class App extends Component {
   render() {
     return (
       <div className="main">
+        <div>
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            alt="netflix_logo"
+          />
+        </div>
         <Row type="My List" />
         <Row type="Recommendations" />
-        <Bottom/>
+        <Bottom />
       </div>
     );
   }
